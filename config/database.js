6565ded65f7f4a7ed99fa24b34711e2003223a1d
lynettenyambura <linetnyambura45@gmail.com>
2024-11-26 15:06:42 +0300

    Remove database.js from tracking

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Lynn', 'root', 'Lynn3745##', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-module.exports = sequelize;
